422c1b91708635c4c170752819eff7a0e66410a9
vikasdesale <vikas.desaleu@gmail.com>
2017-02-19 10:36:46 +0530

    practice floyd_triangle_7

---
+++ c++/basic/7.floyd_triangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int i,j,k=0,rows,count=1;
+
+ cout<<"Enter Rows:";
+ cin>>rows;
+
+ for(i=0;i<=rows;i++){
+
+
+
+    for(int j = 0; j <= i; j++)
+        {
+
+            cout << count++ << "   ";
+        }
+
+        cout<<"\n";
+
+    }
+
+return 0;
+}
