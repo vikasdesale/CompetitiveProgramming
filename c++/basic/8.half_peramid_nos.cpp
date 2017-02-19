b2e0e24d62dfe3821b7968966275ef5b9ac20ffe
vikasdesale <vikas.desaleu@gmail.com>
2017-02-19 10:47:52 +0530

    practice half_peramid_no_9

---
+++ c++/basic/8.half_peramid_nos.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int i,j,k=0,rows;
+
+ cout<<"Enter Rows:";
+ cin>>rows;
+
+ for(i=0;i<=rows;i++){
+
+
+
+    for(int j = 1; j <= i; j++)
+        {
+
+            cout <<j<<" ";
+        }
+
+        cout<<"\n";
+
+    }
+
+return 0;
+}
