db3025181d64714f64cab3e0c7dee3c3c65ec181
vikasdesale <vikas.desaleu@gmail.com>
2017-03-09 23:44:06 +0530

    class CountTheWords

---
+++ Interview/3.Strings/CountTheWords.java
@@ -0,0 +1,15 @@
+class CountTheWords
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the string");
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s=sc.nextLine();
+ 
+        String[] words = s.trim().split(" ");
+ 
+        System.out.println("Number of words in the string = "+words.length);
+    }
+}
