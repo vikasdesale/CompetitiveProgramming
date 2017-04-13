5a1d4a8c87c83b946e44856f4da1711c4181331e
vikasdesale <vikas.desaleu@gmail.com>
2017-04-14 00:03:41 +0530

    DecimalToBinary

---
+++ Interview/12.Number/DecimalToBinary.java
@@ -0,0 +1,18 @@
+public class DecimalToBinary {
+ 
+    public static void printBinaryFormat(int number){
+        int binary[] = new int[25];
+        int index = 0;
+        while(number > 0){
+            binary[index++] = number%2;
+            number = number/2;
+        }
+        for(int i = index-1;i >= 0;i--){
+            System.out.print(binary[i]);
+        }
+    }
+     
+    public static void main(String a[]){
+       printBinaryFormat(25);
+    }
+}
