9dd7a230b131a074c3cbbfcd84313cddaa05fc9b
vikasdesale <vikas.desaleu@gmail.com>
2017-05-20 20:23:37 +0530

    output:2A

---
+++ Interview/1.General/output/2A.java
@@ -0,0 +1,14 @@
+class 2A
+{
+    
+public static void main(String [] args)
+{
+ 
+  String s = "ONE"+3+2+"TWO"+"THREE"+5+4+"FOUR"+"FIVE"+5;
+  System.out.println(s);
+ 
+ }
+ 
+}
+
+//output: ONE32TWOTHREE54FOURFIVE5
