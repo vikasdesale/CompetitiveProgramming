c0df73b9c5a488c35d4d13db844c54b26c6d1853
vikasdesale <vikas.desaleu@gmail.com>
2017-06-13 23:32:00 +0530

    Java

---
+++ Interview/1.General/ImmutableTest.java
@@ -0,0 +1,8 @@
+public class ImmutableTest {
+ public static void main(String ags[]){
+  String initial = "ABCDEFG"; 
+  String after = initial.replace('A', 'Z'); 
+  System.out.println("initial = " + initial); 
+  System.out.println("after= " + after);
+ }
+}
